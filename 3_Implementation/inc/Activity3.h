15366644c6791fab9730af3ee40111d6c677236a
Aarthi-Mamidala <89648059+Aarthi-Mamidala@users.noreply.github.com>
2021-09-17 18:59:49 +0530

    Update Activity3.h

--- 3_Implementation/inc/Activity3.h
+++ 3_Implementation/inc/Activity3.h
@@ -1,6 +1,19 @@
 #ifndef ACTIVITY3_H_INCLUDED
 #define ACTIVITY3_H_INCLUDED
 
+#define PWM_20_PERCENT (205) 
+#define PWM_40_PERCENT (410) 
+#define PWM_70_PERCENT (717) 
+#define PWM_95_PERCENT (973) 
+
+ 
+#include <util/delay.h>
+#include <avr/io.h>
+
+void InitTimer();
+
+void Activity3_PWM(uint16_t temp);
+
 
 
 #endif // ACTIVITY3_H_INCLUDED
