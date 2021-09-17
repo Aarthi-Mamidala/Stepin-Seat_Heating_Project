55684781ac00fc43969fbe29378c5e4a29c01999
Aarthi-Mamidala <89648059+Aarthi-Mamidala@users.noreply.github.com>
2021-09-17 18:57:05 +0530

    Update Activity2.h

--- 3_Implementation/inc/Activity2.h
+++ 3_Implementation/inc/Activity2.h
@@ -7,18 +7,17 @@
 
 //Function Definitions
 
-//@brief Initialize the registers for ADC setup
+// Initialize the registers for ADC setup
 void InitADC();
 
-//@brief Gives the ADC values for the selected channel
+//Gives the ADC values for the selected channel
 
-//@param[in] ch Channel number from 0-7
-//@return uint16_t ADC value
+
 
 uint16_t ReadADC(uint8_t ch);
 
-//@brief Return the ADC value to main function
-//@return uint16_t The ADC value
+// Return the ADC value to main function
+// uint16_t The ADC value
 uint16_t activity2_GetADC(void);
 
 
