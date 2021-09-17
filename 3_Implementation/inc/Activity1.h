5662663fe2ed7e9df5284e344de799a57c904738
Aarthi-Mamidala <89648059+Aarthi-Mamidala@users.noreply.github.com>
2021-09-17 18:56:20 +0530

    Update Activity1.h

--- 3_Implementation/inc/Activity1.h
+++ 3_Implementation/inc/Activity1.h
@@ -1,12 +1,17 @@
 #ifndef ACTIVITY1_H_INCLUDED
 #define ACTIVITY1_H_INCLUDED
 
-#define F_CPU 16000000UL 	// Clock Frequency of MCU is 16 MHz
-#define LED_PORT (PORTD)    // LED Port Number
-#define LED_PIN  (PORTD3)   //LED Pin number
-#define BUTTON_SENSOR  (PORTD1)   // Port for Button Sensor
-#define TEMP_SENSOR  (PORTD2)   // Port for Temperature Sensor
+ 	// Clock Frequency of MCU is 16 MHz
+#define F_CPU 16000000UL
+ // LED Port Number
+#define LED_PORT (PORTD) 
+ //LED Pin number
+#define LED_PIN  (PORTD3) 
+// Port for Button Sensor
+#define BUTTON_SENSOR  (PORTD1) 
+// Port for Temperature Sensor
 
+#define TEMP_SENSOR  (PORTD2)   
 
 #include <util/delay.h>
 #include <avr/io.h>
